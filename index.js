9d5957fc433573a746c0d490d47d43376d43c4a9
Austin Buckley <austinbuckley2023@gmail.com>
2024-06-14 13:35:25 -0700

    added timestamp api

--- index.js
+++ index.js
@@ -19,9 +19,32 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
+
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  if (isInvalidDate(date)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  res.json({
+    unix: isInvalidDate(date) ? null : date.getTime(),
+    utc: isInvalidDate(date) ? null : date.toUTCString(),
+  });
+});
+
+app.get("/api", (_req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
 });
 
 
